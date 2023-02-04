1ade4a3aa5b1b6a5fef2635b970a345a1a799cfc
Muhammed Mustafa Savar <62511949+muffafa@users.noreply.github.com>
2023-02-04 10:46:02 +0000

    0003 solved

--- 0003-longest-substring-without-repeating-characters/lengthOfLongestSubstring.c
+++ 0003-longest-substring-without-repeating-characters/lengthOfLongestSubstring.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    // Your code goes here
-    return 0;
+#define MAX_CHARS 256
+
+int lengthOfLongestSubstring(char* s) {
+    int last_occurrence[MAX_CHARS];
+    int start = 0;
+    int max_len = 0;
+
+    memset(last_occurrence, -1, sizeof(last_occurrence));
+
+    for (int i = 0; s[i]; i++) {
+        if (last_occurrence[s[i]] >= start) { //check for that letter readed before
+            start = last_occurrence[s[i]] + 1;  //if it is slide start point to one right of that point abcd+c =>  dc is new substring
+        }
+        last_occurrence[s[i]] = i;  //update last occurence of that letter
+        max_len = (i - start + 1 > max_len) ? (i - start + 1) : max_len;  //check current is longest or not 
+    }
+
+    return max_len;
 }
 
+int main() {
+    char s[] = "abcdabcdfbbd";
+    printf("Length of the longest substring without repeating characters: %d\n", lengthOfLongestSubstring(s));
+    return 0;
+}
